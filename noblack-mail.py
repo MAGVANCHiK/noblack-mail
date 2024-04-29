bbdc7e22ae46479cea30792bd9e61fbc08132705
MAGVANCHiK <99087756+MAGVANCHiK@users.noreply.github.com>
2024-04-29 08:08:31 +0700

    Update noblack-mail.py

--- noblack-mail.py
+++ noblack-mail.py
@@ -2,19 +2,19 @@ import http.client
 import json
 
 payload = json.dumps({"content": "<@558234730005397510> с др"})
-
-tokens = [str(Input("token1: ")), str(Input("token2: ")), str(Input("token3: "))]
+count = 0
+tokens = [str(input("token1: ")), str(input("token2: ")), str(input("token3: "))]
 
 def sender():
     retry = 0
     # while True:
     for token in tokens:
             conn = http.client.HTTPSConnection("discord.com")
-            conn.request("POST", "/api/v9/channels/1041377842073710624/messages", payload, headers={'Authorization': token, 'Content-Type': "application/json"})
+            conn.request("POST", "/api/v9/channels/1194216308603093114/messages", payload, headers={'Authorization': token, 'Content-Type': "application/json"})
             response = conn.getresponse()
-
             if response.status == 200:
-                print(response.status, token)
+                count+=1
+                print(response.status, token, count)
             else:
                 print(response.status, response.reason)
                 retry += 1
